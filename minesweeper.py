4d02fdab9fcb476aa691539436310abb260b5971
Vaibhav-ac <79320877+Vaibhav-ac@users.noreply.github.com>
2023-01-31 20:46:11 +0530

    Create minesweeper.py

---
+++ minesweeper.py
@@ -0,0 +1,135 @@
+import random
+
+class boardSpot(object):
+
+    def __init__(self):
+        self.value = 0
+        self.selected = False
+        self.mine = False
+
+    def __str__(self):
+        return str(boardSpot.value)
+
+    def isMine(self):
+        if boardSpot.value == -1:
+            return True
+        return False
+
+class boardClass(object):
+    def __init__(self, m_boardSize, m_numMines):
+        self.board = [[boardSpot() for i in range(m_boardSize)] for j in range(m_boardSize)]
+        self.boardSize = m_boardSize
+        self.numMines = m_numMines
+        self.selectableSpots = m_boardSize * m_boardSize - m_numMines
+        i = 0
+        while i < m_numMines:
+            x = random.randint(0, self.boardSize-1)
+            y = random.randint(0, self.boardSize-1)
+            if not self.board[x][y].mine:
+                self.addMine(x, y)
+                i += 1
+
+    def __str__(self):
+        returnString = " "
+        divider = "\n---"
+
+        for i in range(0, self.boardSize):
+            returnString += " | " + str(i)
+            divider += "----"
+        divider += "\n"
+
+        # It labels X-axis
+
+        returnString += divider
+        for y in range(0, self.boardSize):
+            returnString += str(y)
+            # It labels Y-axis
+            for x in range(0, self.boardSize):
+                if self.board[x][y].mine and self.board[x][y].selected:
+                    returnString += " |" + str(self.board[x][y].value)
+                    # It reduces the extra space created by -1
+                elif self.board[x][y].selected:
+                    returnString += " | " + str(self.board[x][y].value)
+                else:
+                    returnString += " |  "
+            returnString += " |"
+            returnString += divider
+        return returnString
+
+    def addMine(self, x, y):
+        self.board[x][y].value = -1
+        self.board[x][y].mine = True
+        # This adds mine to the spot by changing the spot value to -1 and
+        # mine as true
+        for i in range(x-1, x+2):
+            if i >= 0 and i < self.boardSize:
+              # This checks the cordinates remain inbound while we are 
+              # adding the values in the adjacent side of the mine
+                if y-1 >= 0 and not self.board[i][y-1].mine:
+                    self.board[i][y-1].value += 1
+                if y+1 < self.boardSize and not self.board[i][y+1].mine:
+                    self.board[i][y+1].value += 1
+        if x-1 >= 0 and not self.board[x-1][y].mine:
+            self.board[x-1][y].value += 1
+        if x+1 < self.boardSize and not self.board[x+1][y].mine:
+            self.board[x+1][y].value += 1
+
+    def makeMove(self, x, y):
+        self.board[x][y].selected = True
+        self.selectableSpots -= 1
+        # It marks that spot as selected and reduces the number of 
+        # available selectablespots
+        if self.board[x][y].value == -1:
+            return False
+            # It checks whether the spot has mine or not
+        if self.board[x][y].value == 0:
+          # It reduces recursion to check for all the adjacent spots with 
+          # value = 0
+            for i in range(x-1, x+2):
+                if i >= 0 and i < self.boardSize:
+                    if y-1 >= 0 and not self.board[i][y-1].selected:
+                        self.makeMove(i, y-1)
+                    if y+1 < self.boardSize and not self.board[i][y+1].selected:
+                        self.makeMove(i, y+1)
+            if x-1 >= 0 and not self.board[x-1][y].selected:
+                self.makeMove(x-1, y)
+            if x+1 < self.boardSize and not self.board[x+1][y].selected:
+                self.makeMove(x+1, y)
+            return True
+        else:
+          # It checks whether the spot has non-zero and non-negative value 
+          # or not
+            return True
+
+    def hitMine(self, x, y):
+        return self.board[x][y].value == -1
+
+    def isWinner(self):
+        return self.selectableSpots == 0
+
+
+#play game
+def playGame():
+    boardSize = int(input("Choose the Width of the board: "))
+    numMines = int(input("Choose the number of mines: "))
+    gameOver = False
+    winner = False
+    Board = boardClass(boardSize, numMines)
+    while not gameOver:
+        print(Board)
+        print("Make your move:")
+        x = int(input("x: "))
+        y = int(input("y: "))
+        Board.makeMove(x, y)
+        gameOver = Board.hitMine(x, y)
+        if Board.isWinner() and gameOver == False:
+            gameOver = True
+            winner = True
+
+    print(Board)
+    if winner:
+        print("Congratulations, You Win!")
+    else:
+        print("You hit a mine, Game Over!")
+
+playGame()
